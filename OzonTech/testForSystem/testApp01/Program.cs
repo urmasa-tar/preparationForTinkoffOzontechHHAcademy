ba07913ea85a8a31820de621c363cfde3bc3f358
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-07-31 09:34:48 +0300

    with_test_prog

---
+++ OzonTech/testForSystem/testApp01/Program.cs
@@ -0,0 +1,14 @@
+﻿
+class Program
+{
+    public static void Main(string[] args)
+    {
+        using var input = new StreamReader(Console.OpenStandardInput());
+        using var output = new StreamWriter(Console.OpenStandardOutput());
+
+        // var s = input.ReadLine().Split();
+        // int a = int.Parse(s[0]);
+        // int b = int.Parse(s[1]);
+        output.Write("OK");
+    }
+}
