b5b02fe66b5ff1017ce2200ed4d6bddc3ed13508
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-08-03 17:47:56 +0300

    CoastFromShop

---
+++ OzonTech/PriviousYears/Program.cs
@@ -0,0 +1,12 @@
+﻿using PriviousYears;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        marketCoast objTest = new marketCoast();
+
+        // Console.WriteLine(objTest.marketCoastSum());
+        Console.WriteLine(objTest.HashMapCoastSum());
+    }
+}
---
+++ OzonTech/PriviousYears/marketCoast.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriviousYears
+{
+    public class marketCoast
+    {
+        public marketCoast() { }
+
+        public int marketCoastSum()
+        {
+            int res = 0;
+            int[] arrayOfCoast = new int[10] { 1, 1, 1, 1, 3, 1, 1, 1, 2, 1};
+
+            Array.Sort(arrayOfCoast); // n * log(n)
+
+            int inrow = 0;
+            for(int i = 0; i < arrayOfCoast.Length; i++) // O(n)
+            {
+                if (i > 0)
+                {
+                    if ((arrayOfCoast[i] == arrayOfCoast[i - 1]))
+                    {
+                        inrow += 1;
+                    }
+                    else
+                    {
+                        if(inrow >= 3)
+                        {
+                            res += (arrayOfCoast[i - 1] * inrow) - (arrayOfCoast[i - 1] * inrow / 3);
+                        }
+                        else
+                        {
+                            res += arrayOfCoast[i - 1] * inrow;
+                        }
+                        inrow = 1;
+                    }
+                }
+                else
+                {
+                    // res += arrayOfCoast[i];
+                    inrow += 1;
+                    continue;
+                }
+            }
+
+            return res;
+        }
+
+        public int HashMapCoastSum()
+        {
+            int res = 0;
+            int[] arr = new int[10] { 1, 2, 2, 3, 4, 4, 4, 5, 6, 4};
+            Dictionary<int, int> mapOfCoast = new Dictionary<int, int>();
+
+            // O(n)
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (mapOfCoast.ContainsKey(arr[i]))
+                {
+                    mapOfCoast[arr[i]] += 1;
+                }
+                else
+                {
+                    mapOfCoast.Add(arr[i], 1);
+                }
+            }
+
+            foreach (var item in mapOfCoast)
+            {
+                res += (item.Key * item.Value) - (item.Key * (item.Value / 3));
+            }
+            return res;
+        }
+    }
+}
