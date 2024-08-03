75de235cb40f9c70f31e02075051a5478d96a6e2
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-08-03 18:47:12 +0300

    WithSecondTask

---
+++ OzonTech/PriviousYears/adresBook.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriviousYears
+{
+    public class adresBook
+    {
+        public adresBook() { }
+
+        public void callList(int n)
+        {
+            using var input = new StreamReader(Console.OpenStandardInput());
+            Dictionary<string, List<string>> dicCallNumByName = new Dictionary<string, List<string>>();
+
+            // O(n^2)
+            for(int i = 0; i < n; i++)
+            {
+                var st_row = input.ReadToEnd().Split(' ');
+                string personName = st_row[0];
+                string personNumb = st_row[1];
+
+                if (dicCallNumByName.ContainsKey(personName))
+                {
+                    bool will = true;
+                    foreach(string st in dicCallNumByName[personName])
+                    {
+                        if(st == personNumb)
+                        {
+                            will = false;
+                            break;
+                        }
+                    }
+                    if(will)dicCallNumByName[personName].Add(personNumb);
+                }
+                else
+                {
+                    dicCallNumByName[personName] = new List<string>() { personNumb };
+                }
+            }
+        }
+
+        public void writeLastByName(string Name, Dictionary<string, List<string>> dicCallNumByName)
+        {
+            if (dicCallNumByName[Name].Count >= 5){
+                for(int i = 1; i < 6; i++)
+                {
+                    Console.Write(dicCallNumByName[Name][dicCallNumByName[Name].Count - i]);
+                    if (i != 5) Console.Write(" ");
+                    else Console.Write("\n");
+                }
+            }
+            else
+            {
+                for (int i = dicCallNumByName[Name].Count - 1; i >= 0; i--)
+                {
+                    Console.Write(dicCallNumByName[Name][i]);
+                    if (i != 0) Console.Write(" ");
+                    else Console.Write("\n");
+                }
+            }
+        }
+    }
+}
---
+++ OzonTech/PriviousYears/loginCheck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PriviousYears
+{
+    public class loginCheck
+    {
+        public bool[] registrationSys(int n)
+        {
+            bool[] res = new bool[n];
+            List<string> list = new List<string>();
+
+            for(int i = 0; i < n; i++)
+            {
+                string patternForReg = "@()()";
+            }
+            return res;
+        }
+    }
+}
